7560f4714b255e0b7c51aad1d381c0bf802c8738
Kamil Stróżyk <strozykkamil@gmail.com>
2020-04-17 10:32:13 +0200

    add simple sequential code

---
+++ OpenMPProject/OpenMPProject/SimpleSequential.cpp
@@ -0,0 +1,23 @@
+#include<math.h>
+#include <stdio.h>
+#define M 4
+#define N 21370
+
+
+int main()
+{
+	for (int i = M; i < N; i++)
+	{
+		bool hasDivider = false;
+		for (int j = 2; j < sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				hasDivider = true;
+				break;
+			}
+		}
+		if (!hasDivider)
+			printf("%d\n", i);
+	}
+}
