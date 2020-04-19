28fe444e1f159ea129e89b3247893928239ead24
rolex <drolewski@gmail.com>
2020-04-19 23:52:37 +0200

    Add domen solution :)

--- OpenMPProject/OpenMPProject/SequenceSolution.cpp
+++ OpenMPProject/OpenMPProject/SequenceSolution.cpp
@@ -1,11 +1,13 @@
 #include<math.h>
 #include <stdio.h>
+#include <omp.h>
 #define M 2
 #define N 49
 
 
 int generatePrimeNumbersArray(int numbers[]) {
 	int iterator = 0;
+#pragma omp parallel for
 	for (int i = M; i < N; i++)
 	{
 		bool hasDivider = false;
@@ -26,6 +28,7 @@ int generatePrimeNumbersArray(int numbers[]) {
 }
 
 bool checkIsNumberPrimeWithArray(int primeNumbers[], int iterator, int number) {
+#pragma omp parallel for
 	for (int i = 0; i < iterator; i++) {
 		if (number % primeNumbers[i] == 0 && primeNumbers[i] < number)
 		{
@@ -36,6 +39,7 @@ bool checkIsNumberPrimeWithArray(int primeNumbers[], int iterator, int number) {
 }
 
 bool checkDivisionBySmallerNumbers(int number) {
+#pragma omp parallel for
 	for (int i = 2; i < number; i++)
 	{
 		if (number % i == 0)
@@ -47,6 +51,7 @@ bool checkDivisionBySmallerNumbers(int number) {
 }
 
 bool checkEratosthenesSieve(int primeNumbers[], int iterator, int number) {
+#pragma omp parallel for
 	for (int i = 0; i < iterator; i++) {
 		for (int j = 2; j < primeNumbers[i]; j++) {
 			for (int k = 2; k < j; k++) {
@@ -64,13 +69,48 @@ bool checkEratosthenesSieve(int primeNumbers[], int iterator, int number) {
 
 int main()
 {
+	// procesy otrzymuj¹ ca³¹ tablicê liczb pierwszych -> generowana w sekcji ka¿dego w¹tku, podejscie domenowe,
+	// rozumiem je tak ze tablica wykreœleñ jest wspó³dzielona, a tablica liczb pierwszych oddzielna dla ka¿dego w¹tku
+	/*
+	int solution[N / 2];
+	int iter = 0;
+	omp_set_num_threads(4);
+#pragma omp parallel
+	{
+		int data[N / 2] = { 0 };
+		int iterator = generatePrimeNumbersArray(data);
+#pragma omp parallel for
+		for (int i = M; i < N; i++) {
+			//if (checkIsNumberPrimeWithArray(data, iterator, i)) { // with array
+			//if(checkDivisionBySmallerNumbers(i)){ // division by smaller number
+			if (checkEratosthenesSieve(data, iterator, i)) {
+				solution[iter] = i;
+				++iter;
+			}
+		}
+	}*/
+
+	
+	int iter = 0;
 	int data[N / 2] = { 0 };
 	int iterator = generatePrimeNumbersArray(data);
-	for (int i = M; i < N; i++) {
-		//if (checkIsNumberPrimeWithArray(data, iterator, i)) { // with array
-		//if(checkDivisionBySmallerNumbers(i)){ // division by smaller number
-		if(checkEratosthenesSieve(data, iterator, i)){
-			printf("Prime number: %d\n", i);
+	omp_set_num_threads(4);
+#pragma omp parallel
+	{
+		int id = omp_get_thread_num();
+		int solution[N / 2];
+#pragma omp parallel for private(solution)
+		for (int i = M; i < N; i++) {
+			//if (checkIsNumberPrimeWithArray(data, iterator, i)) { // with array
+			//if(checkDivisionBySmallerNumbers(i)){ // division by smaller number
+			if (checkEratosthenesSieve(data, iterator, i)) {
+				solution[iter] = i;
+				++iter;
+			}
+		}
+		for (int i = 0; i < iter; i++) {
+			printf("id %d - sol: %d\n", id, solution[i]);
 		}
 	}
+	
 }
